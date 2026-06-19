87ec24f87fd457e0cdc11427a6fb060234c7bac8
sirisha <atavalisirisha09@gmail.com>
2026-06-19 11:54:17 -0700

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/ShopLite-React-E-Commerce-App">
     <App />
   </BrowserRouter>
 );
